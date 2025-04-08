7a2d6aaf6db195e1c00690a7d41a0dcf2ba52d7d
Guy <guy2006av@gmail.com>
2025-04-08 12:58:25 +0300

    working sim game board and logic

---
+++ src/SIM/models/SimGraph.java
@@ -0,0 +1,49 @@
+package SIM.models;
+
+import java.util.Arrays;
+
+public class SimGraph {
+    private PlayerCode[][] board; // A 2D-array representaion of the game graph
+    private int numOfVertices;
+
+    public SimGraph(int numOfVertices) {
+        this.numOfVertices = numOfVertices;
+        board = new PlayerCode[numOfVertices][numOfVertices];
+
+        reset();
+    }
+
+    // If source != destination and connection has not been done- connect the two.
+    // then check for a winning player and return his code
+    public PlayerCode connect(int source, int destination, PlayerCode playerCode) {
+        if (board[source][destination] != PlayerCode.None || source == destination)
+            return null; // returns null if there's an error with current move
+
+        board[source][destination] = playerCode;
+        board[destination][source] = playerCode;
+
+        // Check if move was game ending
+        for (int i = 0; i < numOfVertices; i++) {
+            if (source == i || destination == i)
+                continue;
+
+            if (board[source][i] == playerCode && board[destination][i] == playerCode) {
+                // Returns the winner player's code in case the current one created a triangle
+                return playerCode == PlayerCode.PlayerOne ? PlayerCode.PlayerTwo : PlayerCode.PlayerOne;
+            }
+        }
+
+        // Returns Player Code none if no one won
+        return PlayerCode.None;
+    }
+
+    public PlayerCode[][] getBoard() {
+        return board;
+    }
+
+    public void reset() {
+        for (int i = 0; i < numOfVertices; i++) {
+            Arrays.fill(board[i], PlayerCode.None);
+        }
+    }
+}
