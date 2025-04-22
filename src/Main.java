1944a16682e11aa4846cbc7659d449fd993c55a3
Guy <guy2006av@gmail.com>
2025-04-22 12:14:52 +0300

    new UI- file picking bug

--- src/Main.java
+++ src/Main.java
@@ -5,58 +5,73 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-public class Main extends JFrame 
-{
-	
-	public void menu()
-	{
-		String[] options =  {"Easy Encryption 1", "Easy Decryption 1", "Exit"};
-		int response = JOptionPane.showOptionDialog(null, "Options: ", 
+public class Main extends JFrame {
+
+	public void menu() {
+		String[] options = { "Dijkstra's Encryption", "Dijkstra's Decryption", "SIM Encryption", "SIM Decryption",
+				"Gould's Encryption", "Gould's Decryption", "Exit" };
+		int response = JOptionPane.showOptionDialog(null, "Options: ",
 				"Encryption Menu",
 				JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
 				new ImageIcon("assets/image.png"), options, options[0]);
-		
-		switch( response)
-		{
-			case -1:
-				System.exit(0);
-			
-			case 0:
-				JFileChooser ch0 = new JFileChooser();
-			    int r0 = ch0.showOpenDialog(this);
-			    if (r0 == JFileChooser.APPROVE_OPTION) 
-			    {
-			    	File f = ch0.getSelectedFile();
-			    	EasyEncryption ee = new EasyEncryption();
-				    ee.easyEncryptFile(f);  	
-			    }
-			    menu();
-				break;
-				
-			case 1:
-				 JFileChooser ch1 = new JFileChooser();
-			     int r1 = ch1.showOpenDialog(this);
-			     if (r1 == JFileChooser.APPROVE_OPTION) 
-			     {
-			    	File f = ch1.getSelectedFile();
-			    	EasyEncryption ee = new EasyEncryption();
-				    ee.easyDecryptFile(f);    	
-			     } 	
-			     menu();
-				break;
-				
-			
-			case  2:
-				System.exit(0);
-				
-			default:
-				break;		
+
+		JFileChooser ch0 = new JFileChooser();
+		int r0 = ch0.showOpenDialog(this);
+		if (r0 == JFileChooser.APPROVE_OPTION) {
+			File f = ch0.getSelectedFile();
+			EasyEncryption ee = new EasyEncryption();
+
+			switch (response) {
+				case -1:
+					System.exit(0);
+
+				case 0:
+					ee.easyEncryptFile(f); // TODO : replace with respective encryption function
+
+					menu();
+					break;
+
+				case 1:
+					ee.easyDecryptFile(f); // TODO : replace with respective decryption function
+
+					menu();
+					break;
+
+				case 2:
+					ee.easyEncryptFile(f); // TODO : replace with respective encryption function
+
+					menu();
+					break;
+
+				case 3:
+					ee.easyDecryptFile(f); // TODO : replace with respective decryption function
+
+					menu();
+					break;
+
+				case 4:
+					ee.easyEncryptFile(f); // TODO : replace with respective encryption function
+
+					menu();
+					break;
+
+				case 5:
+					ee.easyDecryptFile(f); // TODO : replace with respective decryption function
+
+					menu();
+					break;
+
+				case 6:
+					System.exit(0);
+
+				default:
+					break;
+			}
 		}
+
 	}
-	
-	
-	public static void main(String[] args) 
-	{
+
+	public static void main(String[] args) {
 		Main m = new Main();
 		m.menu();
 
